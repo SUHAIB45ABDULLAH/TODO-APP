118f2bda56e05e8762901660b4b13233687bfd6e
SUHAIB45ABDULLAH <suhaibabdullah69@gmail.com>
2025-02-08 01:14:32 +0500

    Updated

--- App.jsx
+++ App.jsx
@@ -1,20 +1,45 @@
+import { useState } from "react";
+import AddTodo from "./COMPONENTS/AddTodo";
 import Navbar from "./COMPONENTS/Navbar";
-import TodoItem from "./COMPONENTS/TodoItem";
 import TodoList from "./COMPONENTS/TodoList";
-import './index.css';
-
-
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const handleAdd = (newTodo) => {
+    const newTodoItem = { id: Date.now().toString(), todo: newTodo, isCompleted: false };
+    setTodos([...todos, newTodoItem]);
+  };
+
+  const handleDelete = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  const handleEdit = (id, newText) => {
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, todo: newText } : todo));
+  };
+
+  const handleCheckbox = (id) => {
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo));
+  };
+
   return (
     <>
       <Navbar />
       <div className="bg-red-500 text-white p-4">
-  Tailwind is working!
-</div>
+        Tailwind is working!
+      </div>
 
-      <TodoList />
-      <TodoItem/>
+      {/* TodoList ko sahi props pass karo */}
+      <TodoList 
+        todos={todos} 
+        handleEdit={handleEdit} 
+        handleDelete={handleDelete} 
+        handleCheckbox={handleCheckbox} 
+      />
+
+      {/* AddTodo ko handleAdd pass karo */}
+      <AddTodo handleAdd={handleAdd} />
     </>
   );
 }
